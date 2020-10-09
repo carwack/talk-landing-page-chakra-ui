20176b0a087f5d3d61c56e24de2eea5f077bd2b7
carwack <sybren.willemot@gmail.com>
2020-10-09 10:53:06 +0200

    💄 Keep CThemeProvider

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
-    <c-color-mode-provider>
-      <c-box font-family="body" as="main">
-        <c-reset />
-        <nuxt />
-      </c-box>
-    </c-color-mode-provider>
+    <c-theme-provider>
+      <c-color-mode-provider>
+        <c-box font-family="body" as="main">
+          <c-reset />
+          <nuxt />
+        </c-box>
+      </c-color-mode-provider>
+    </c-theme-provider>
   </div>
 </template>
 <script>
